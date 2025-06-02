cb8a9934bdde5d631ff943432132b438a2a68270
wahjoe16 <46861259+wahjoe16@users.noreply.github.com>
2025-06-02 14:45:17 +0700

    update admin panel theme

--- resources/views/sidebar_menu/dekanat.blade.php
+++ resources/views/sidebar_menu/dekanat.blade.php
@@ -1,63 +1,91 @@
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-street-view"></i>
-        <span>SKKFT</span>
-        <span class="pull-right-container">
-        <i class="fa fa-angle-left pull-right"></i>
-        </span>
+<li class="nav-section">
+    <span class="sidebar-mini-icon">
+        <i class="fa fa-ellipsis-h"></i>
+    </span>
+    <h4 class="text-section">SKKFT</h4>
+</li>
+<li class="nav-item {{ Route::is('dashboardSkkft.index') || Route::is('dataSkkft.index')
+                    || Route::is('sertifikat.index') || Route::is('sertifikat.show') ? 'active' : '' }}">
+    <a data-bs-toggle="collapse" href="#skkft-superadmin">
+        <i class="fas fa-bolt"></i>
+        <p>SKKFT</p>
+        <span class="caret"></span>
     </a>
-    <ul class="treeview-menu">
-        <li>
-            <a href="" @if(Session::get('page')=='indexKegiatanSkkft') style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-leanpub"></i> <span>Data SKKFT</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('sertifikat.index') }}" @if(Session::get('page')=='summaryKegiatanSkkft') style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-database"></i> <span>Sertifikat SKKFT</span>
-            </a>
-        </li>
-    </ul>
+    <div class="collapse {{ Route::is('dashboardSkkft.index') || Route::is('dataSkkft.index')
+                         || Route::is('sertifikat.index') || Route::is('sertifikat.show') ? 'show' : '' }}" id="skkft-superadmin">
+        <ul class="nav nav-collapse">
+            <li class="{{ Route::is('dashboardSkkft.index') || Route::is('dataSkkft.index') ? 'active' : '' }}">
+                <a href="{{ route('dataSkkft.index') }}">
+                    <span class="sub-item">Data SKKFT</span>
+                </a>
+            </li>
+            <li class="{{ Route::is('sertifikat.index') || Route::is('sertifikat.show') ? 'active' : '' }}">
+                <a href="{{ route('sertifikat.index') }}">
+                    <span class="sub-item">Sertifikat SKKFT</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </li>
 
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-newspaper-o"></i>
-        <span>SKPI</span>
-        <span class="pull-right-container">
-        <i class="fa fa-angle-left pull-right"></i>
-        </span>
+
+<li class="nav-section">
+    <span class="sidebar-mini-icon">
+        <i class="fa fa-ellipsis-h"></i>
+    </span>
+    <h4 class="text-section">SKPI</h4>
+</li>
+<li class="nav-item {{ Route::is('skpi.index') || Route::is('skpi.list')
+                    || Route::is('skpi.show') ? 'active' : '' }}">
+    <a data-bs-toggle="collapse" href="#skpi-superadmin">
+        <i class="fas fa-file-alt"></i>
+        <p>SKPI</p>
+        <span class="caret"></span>
     </a>
-    <ul class="treeview-menu">
-        <li>
-            <a href="{{ route('skpi.index') }}" @if(Session::get('page')=='') style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-leanpub"></i> <span>Pengajuan SKPI</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('skpi.list') }}" @if(Session::get('page')=='') style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-database"></i> <span>Data SKPI</span>
-            </a>
-        </li>
-    </ul>
+    <div class="collapse {{ Route::is('skpi.index') || Route::is('skpi.list')
+                         || Route::is('skpi.show') ? 'show' : '' }}" id="skpi-superadmin">
+        <ul class="nav nav-collapse">
+            <li class="{{ Route::is('skpi.index') || Route::is('skpi.show') ? 'active' : '' }}">
+                <a href="{{ route('skpi.index') }}">
+                    <span class="sub-item">Pengajuan SKPI</span>
+                </a>
+            </li>
+            <li class="{{ Route::is('skpi.list') ? 'active' : '' }}">
+                <a href="{{ route('skpi.list') }}">
+                    <span class="sub-item">Database SKPI</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </li>
 
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-male"></i>
-        <span>Alumni</span>
-        <span class="pull-right-container">
-        <i class="fa fa-angle-left pull-right"></i>
-        </span>
+
+<li class="nav-section">
+    <span class="sidebar-mini-icon">
+        <i class="fa fa-ellipsis-h"></i>
+    </span>
+    <h4 class="text-section">Alumni</h4>
+</li>
+<li class="nav-item {{ Route::is('alumni.index') || Route::is('masukan-alumni.index') 
+                    || Route::is('alumni.show') || Route::is('reset-password-alumni.index') ? 'active' : '' }}">
+    <a data-bs-toggle="collapse" href="#alumni-admin">
+        <i class="fas fa-user-tie"></i>
+        <p>Alumni</p>
+        <span class="caret"></span>
     </a>
-    <ul class="treeview-menu">
-        <li>
-            <a href="{{ route('alumni.index') }}" @if(Session::get('page')=='indexAlumni' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-database"></i> <span>Data Alumni</span>
-            </a>
-            <a href="{{ route('masukan-alumni.index') }}" @if(Session::get('page')=='indexAlumni' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-long-arrow-down"></i> <span>Saran & Masukan Alumni</span>
-            </a>
-        </li>
-    </ul>
+    <div class="collapse {{ Route::is('alumni.index') || Route::is('masukan-alumni.index')
+                         || Route::is('alumni.show') ? 'show' : '' }}" id="alumni-admin">
+        <ul class="nav nav-collapse">
+            <li class="{{ Route::is('alumni.index') || Route::is('alumni.show') ? 'active' : '' }}">
+                <a href="{{ route('alumni.index') }}">
+                    <span class="sub-item">Data Alumni</span>
+                </a>
+            </li>
+            <li class="{{ Route::is('masukan-alumni.index') ? 'active' : '' }}">
+                <a href="{{ route('masukan-alumni.index') }}">
+                <span class="sub-item">Saran & Masukan Alumni</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </li>
--- resources/views/sidebar_menu/dosen.blade.php
+++ resources/views/sidebar_menu/dosen.blade.php
@@ -1,106 +1,180 @@
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-graduation-cap"></i>
-        <span>Pelaksanaan Sidang</span>
-        <span class="pull-right-container">
-        <i class="fa fa-angle-left pull-right"></i>
-        </span>
-    </a>
-    <ul class="treeview-menu">
-        {{-- Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Tambang --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan' && auth()->user()->status_koordinator_skripsi == 1)
-            @include('sidebar_menu.koordinator_skripsi_tmb')
-        @endif
-        {{-- Akhir Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Tambang --}}
-        
-        {{-- Menu Pelaksanaan Sidang Untuk Kaprodi Tambang --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan' && auth()->user()->status_kaprodi == 1 || auth()->user()->status_sekprodi == 1)
-            @include('sidebar_menu.kaprodi_sekprodi_tmb')
-        @endif
-        {{-- Akhir Menu Pelaksanaan Sidang Untuk Kaprodi Tambang --}}
-        
-        {{-- Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Industri --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri' && auth()->user()->status_koordinator_skripsi == 1)
-            @include('sidebar_menu.koordinator_skripsi_ti')
-        @endif
-        {{-- Akhir Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Industri --}}
-        
-        {{-- Menu Pelaksanaan Sidang Untuk Kaprodi Teknik Industri --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri' && auth()->user()->status_kaprodi == 1 || auth()->user()->status_sekprodi == 1)
-            @include('sidebar_menu.kaprodi_sekprodi_ti')
-        @endif
-        {{-- Akhir Menu Pelaksanaan Sidang Untuk Kaprodi Teknik Industri --}}
-        
-        {{-- Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Perencanaan Wilayah dan Kota --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota' && auth()->user()->status_koordinator_skripsi == 1)
-            @include('sidebar_menu.koordinator_skripsi_pwk')
-        @endif
-        {{-- Akhir Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Perencanaan Wilayah dan Kota --}}
+{{-- Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Tambang --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan' && auth()->user()->status_koordinator_skripsi == 1)
+    @include('sidebar_menu.koordinator_skripsi_tmb')
+@endif
 
-        {{-- Menu Pelaksanaan Sidang Untuk Kaprodi Perencanaan Wilayah dan Kota --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota' && auth()->user()->status_kaprodi == 1 || auth()->user()->status_sekprodi == 1)
-            @include('sidebar_menu.kaprodi_sekprodi_pwk')
-        @endif
-        {{-- Akhir Menu Pelaksanaan Sidang Untuk Kaprodi Perencanaan Wilayah dan Kota --}}
-        
-        {{-- Menu Pembimbingan Dosen Tambang --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan')
-            <li>
-                <a href="{{ route('bimbinganTmb.index') }}" @if(Session::get('page')=='bimbinganTmb' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                    <i class="fa fa-users"></i> <span>Data Mahasiswa Bimbingan</span>
-                </a>
-            </li>
-        @endif
-        {{-- Akhir Menu Pembimbingan Dosen Tambang --}}
-        
-        {{-- Menu Pembimbingan Dosen TI --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri')
-            <li>
-                <a href="{{ route('bimbinganTi.index') }}" @if(Session::get('page')=='bimbinganTi' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                    <i class="fa fa-users"></i> <span>Data Mahasiswa Bimbingan</span>
-                </a>
-            </li>
-        @endif
-        {{-- Akhir Menu Pembimbingan Dosen TI --}}
+{{-- Menu Pelaksanaan Sidang Untuk Kaprodi Tambang --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan' && auth()->user()->status_kaprodi == 1)
+    @include('sidebar_menu.kaprodi_sekprodi_tmb')
+@endif
 
-        {{-- Menu Pembimbingan Dosen PWK --}}
-        @if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota')
-            <li>
-                <a href="{{ route('bimbinganPwk.index') }}" @if(Session::get('page')=='bimbinganPwk' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                    <i class="fa fa-users"></i> <span>Data Mahasiswa Bimbingan</span>
+{{-- Menu Pelaksanaan Sidang Untuk Sekprodi Tambang --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan' && auth()->user()->status_sekprodi == 1)
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Data Lulusan</h4>
+    </li>
+
+    <li class="nav-item {{ Route::is('rekap-sidangTmb.index') ? 'active' : '' }}">
+        <a href="{{ route('rekap-sidangTmb.index') }}">
+            <i class="fas fa-clipboard-list"></i>
+            <p>Rekap Lulusan</p>
+            <span class="badge badge-secondary"></span>
+        </a>
+    </li>
+@endif
+
+{{-- Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Industri --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri' && auth()->user()->status_koordinator_skripsi == 1)
+    @include('sidebar_menu.koordinator_skripsi_ti')
+@endif
+
+
+{{-- Menu Pelaksanaan Sidang Untuk Kaprodi Teknik Industri --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri' && auth()->user()->status_kaprodi == 1)
+    @include('sidebar_menu.kaprodi_sekprodi_ti')
+@endif
+
+{{-- Menu Pelaksanaan Sidang Untuk Sekprodi TI --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri' && auth()->user()->status_sekprodi == 1)
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Data Lulusan</h4>
+    </li>
+
+    <li class="nav-item {{ Route::is('rekap-sidangTi.index') ? 'active' : '' }}">
+        <a href="{{ route('rekap-sidangTi.index') }}">
+            <i class="fas fa-clipboard-list"></i>
+            <p>Rekap Lulusan</p>
+            <span class="badge badge-secondary"></span>
+        </a>
+    </li>
+@endif
+
+{{-- Menu Pelaksanaan Sidang Untuk Koordinator Skripsi Perencanaan Wilayah dan Kota --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota' && auth()->user()->status_koordinator_skripsi == 1)
+    @include('sidebar_menu.koordinator_skripsi_pwk')
+@endif
+
+
+{{-- Menu Pelaksanaan Sidang Untuk Kaprodi Perencanaan Wilayah dan Kota --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota' && auth()->user()->status_kaprodi == 1)
+    @include('sidebar_menu.kaprodi_sekprodi_pwk')
+@endif
+
+{{-- Menu Pelaksanaan Sidang Untuk Sekprodi PWK --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota' && auth()->user()->status_sekprodi == 1)
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Data Lulusan</h4>
+    </li>
+
+    <li class="nav-item {{ Route::is('rekap-sidangPwk.index') ? 'active' : '' }}">
+        <a href="{{ route('rekap-sidangPwk.index') }}">
+            <i class="fas fa-clipboard-list"></i>
+            <p>Rekap Lulusan</p>
+            <span class="badge badge-secondary"></span>
+        </a>
+    </li>
+@endif
+
+
+{{-- Menu Pembimbingan Dosen Tambang --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Pertambangan')
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Data Bimbingan</h4>
+    </li>
+    <li class="nav-item {{ Route::is('bimbinganTmb.index') || Route::is('bimbinganTmb.showTmb1') || Route::is('bimbinganTmb.showTmb2') ? 'active' : '' }}">
+        <a href="{{ route('bimbinganTmb.index') }}">
+            <i class="fas fa-user-check"></i>
+            <p>Data Mahasiswa Bimbingan</p>
+            <span class="badge badge-secondary"></span>
+        </a>
+    </li>
+@endif
+
+{{-- Menu Pembimbingan Dosen TI --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Teknik Industri')
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Data Bimbingan</h4>
+    </li>
+    <li class="nav-item {{ Route::is('bimbinganTi.index') || Route::is('bimbinganTi.showTi1') || Route::is('bimbinganTi.showTi2') ? 'active' : '' }}">
+        <a href="{{ route('bimbinganTi.index') }}">
+            <i class="fas fa-user-check"></i>
+            <p>Data Mahasiswa Bimbingan</p>
+            <span class="badge badge-secondary"></span>
+        </a>
+    </li>
+@endif
+
+{{-- Menu Pembimbingan Dosen PWK --}}
+@if (auth()->user()->level == 2 && auth()->user()->program_studi == 'Perencanaan Wilayah dan Kota')
+    <li class="nav-section">
+        <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+        </span>
+        <h4 class="text-section">Data Bimbingan</h4>
+    </li>
+    <li class="nav-item {{ Route::is('bimbinganPwk.index') || Route::is('bimbinganPwk.showPwk1') || Route::is('bimbinganPwk.showPwk2') ? 'active' : '' }}">
+        <a href="{{ route('bimbinganPwk.index') }}">
+            <i class="fas fa-user-check"></i>
+            <p>Data Mahasiswa Bimbingan</p>
+            <span class="badge badge-secondary"></span>
+        </a>
+    </li>
+@endif
+
+
+<li class="nav-section">
+    <span class="sidebar-mini-icon">
+        <i class="fa fa-ellipsis-h"></i>
+    </span>
+    <h4 class="text-section">Arsip</h4>
+</li>
+<li class="nav-item {{ Route::is('all-archive.index') || Route::is('my-archive.general') 
+                    || Route::is('my-archive.show') || Route::is('my-archive.index')
+                    || Route::is('my-archive.create') || Route::is('my-archive.edit') ? 'active' : '' }}">
+    <a data-bs-toggle="collapse" href="#my-arsip-dosen">
+        <i class="fas fa-folder-open"></i>
+        <p>Data Arsip</p>
+        <span class="caret"></span>
+    </a>
+    <div class="collapse {{ Route::is('all-archive.index') || Route::is('my-archive.show')
+                         || Route::is('my-archive.general') || Route::is('my-archive.index')
+                         || Route::is('my-archive.create') || Route::is('my-archive.edit') ? 'show' : '' }}" id="my-arsip-dosen">
+        <ul class="nav nav-collapse">
+            <li class="{{ Route::is('all-archive.index') || Route::is('my-archive.show') ? 'active' : '' }}">
+                <a href="{{ route('all-archive.index') }}">
+                    <span class="sub-item">Semua Arsip Fakultas</span>
                 </a>
             </li>
-        @endif
-        {{-- Akhir Menu Pembimbingan Dosen PWK --}}
-                
-    </ul>
+            <li class="{{ Route::is('my-archive.general') ? 'active' : '' }}">
+                <a href="{{ route('my-archive.general') }}">
+                <span class="sub-item">Arsip Umum Fakultas</span>
+                </a>
+            </li>
+            <li class="{{ Route::is('my-archive.index') || Route::is('my-archive.show')
+                       || Route::is('my-archive.create') || Route::is('my-archive.edit') ? 'active' : '' }}">
+                <a href="{{ route('my-archive.index') }}">
+                <span class="sub-item">Arsip Saya</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </li>
-<li class="treeview">
-    <a href="#">
-        <i class="fa fa-book"></i>
-        <span>Arsip Fakultas</span>
-        <span class="pull-right-container">
-        <i class="fa fa-angle-left pull-right"></i>
-        </span>
-    </a>
-    <ul class="treeview-menu">
-        <li>
-            <a href="{{ route('all-archive.index') }}" @if(Session::get('page')=='allArchive' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-book"></i> <span> Semua Arsip</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('my-archive.general') }}" @if(Session::get('page')=='generalArsip' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-file"></i> <span>Arsip Umum Fakultas</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{ route('my-archive.index') }}" @if(Session::get('page')=='myArchive' ) style="background: #3c8dbc !important; color:white !important" @endif>
-                <i class="fa fa-folder"></i> <span>Arsip Saya</span>
-            </a>
-        </li>
-    </ul>
-</li>
+
 
 {{-- Menu khusus untuk dosen pejabar dekanat --}}
 @if (auth()->user()->level == 2 && auth()->user()->status_dekanat == 1)
