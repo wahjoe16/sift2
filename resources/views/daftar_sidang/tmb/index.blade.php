d9122282ce525bc4cb365fe7beb624bd76dfded8
wahjoe16 <46861259+wahjoe16@users.noreply.github.com>
2025-06-02 14:33:52 +0700

    update admin panel theme

--- resources/views/daftar_sidang/tmb/index.blade.php
+++ resources/views/daftar_sidang/tmb/index.blade.php
@@ -1,73 +1,71 @@
-@extends('layouts.master')
+@extends('layouts.dashboard')
 
 @section('content')
 
-<section class="content">
-    @includeIf('layouts.alert')
-    <div class="row">
-        <div class="col-md-12 col-sm-12">
-            <div class="box box-warning">
-                <div class="box-header with-border">
-                    <div class="btn-group">
-                        @if (is_null($dataLogSeminar) || !is_null($dataLogSidang) || !$dataSeminar || is_null($dataSertifikatSkkft))
-                        <a href="#" class="btn btn-success btn-sm btn-flat disabled"><i class="fa fa-upload"></i> Ajukan</a>
-                        @elseif (is_null($dataLogSidang) || $dataSeminar)
-                        <a href="{{ route('sidang_tmb.daftar') }}" class="btn btn-success btn-sm btn-flat"><i class="fa fa-upload"></i> Ajukan</a>
-                        @endif
-                    </div>
+<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
+    <div>
+        <h3 class="fw-bold">Sidang Skripsi</h3>
+    </div>
+</div>
+
+@include('layouts.alert')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="btn-group">
+                    @if (is_null($dataLogSeminar) || !is_null($dataLogSidang) || !$dataSeminar || is_null($dataSertifikatSkkft))
+                    {{-- @if (is_null($dataLogSeminar) || !is_null($dataLogSidang) || !$dataSeminar) --}}
+                    <a href="#" class="btn btn-success btn-sm disabled"><i class="fas fa-upload"></i> Ajukan</a>
+                    @elseif (is_null($dataLogSidang) || $dataSeminar)
+                    <a href="{{ route('sidang_tmb.daftar') }}" class="btn btn-success btn-sm"><i class="fas fa-upload"></i> Ajukan</a>
+                    @endif
                 </div>
-                <div class="box-body table-responsive">
-                    <table class="table table-striped table-bordered table-sidang">
-                        <thead>
-                            <tr>
-                                <th>Tahun Akademik</th>
-                                <th>Semester</th>
-                                <th>Pembimbing</th>
-                                <th>Co. Pembimbing</th>
-                                <th>Tanggal Pengajuan</th>
-                                <th>Status</th>
-                                <th width="15%"><i class="fa fa-cogs"></i> Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($dataSidang as $d)
-                            <tr>
-                                <td>{{ $d->tahun_ajaran->tahun_ajaran }}</td>
-                                <td>{{ $d->semester->semester }}</td>
-                                <td>{{ $d->dosen_1->nama }}</td>
-                                @if ($d->dosen_2 != '')
-                                <td>{{ $d->dosen_2->nama }}</td>
-                                @else
-                                <td>-</td>
-                                @endif
+            </div>
+            <div class="card-body">
+                <table class="table table-striped table-sidang">
+                    <thead>
+                        <tr>
+                            <th>Tahun Akademik</th>
+                            <th>Semester</th>
+                            <th>Pembimbing</th>
+                            <th>Co. Pembimbing</th>
+                            <th>Tanggal Pengajuan</th>
+                            <th>Status</th>
+                            <th width="15%"><i class="fa fa-cogs"></i> Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($dataSidang as $d)
+                        <tr>
+                            <td>{{ $d->tahun_ajaran->tahun_ajaran }}</td>
+                            <td>{{ $d->semester->semester }}</td>
+                            <td>{{ $d->dosen_1->nama }}</td>
+                            @if ($d->dosen_2 != '')
+                            <td>{{ $d->dosen_2->nama }}</td>
+                            @else
+                            <td>-</td>
+                            @endif
 
-                                <td>{{ tanggal_indonesia($d->created_at, false) }}</td>
+                            <td>{{ tanggal_indonesia($d->created_at, false) }}</td>
 
-                                @if ($d->status == 0)
-                                <td><span class="label bg-yellow text-black">Menunggu</span></td>
-                                @elseif ($d->status == 1)
-                                <td><span class="label bg-green">Diterima</span></td>
-                                @elseif ($d->status == 2)
-                                <td><span class="label bg-red">Ditolak</span></td>
-                                @endif
+                            @if ($d->status == 0)
+                            <td><span class="badge badge-warning text-black">Menunggu</span></td>
+                            @elseif ($d->status == 1)
+                            <td><span class="badge badge-success">Diterima</span></td>
+                            @elseif ($d->status == 2)
+                            <td><span class="badge badge-danger">Ditolak</span></td>
+                            @endif
 
-                                <td><a href="{{ route('sidang_tmb.show', $d->id) }}"><i class="fa fa-search"></i></a></td>
-                                @endforeach
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                            <td><a href="{{ route('sidang_tmb.show', $d->id) }}"><i class="fas fa-search"></i></a></td>
+                            @endforeach
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
-</section>
-
-@includeIf('mahasiswa.form')
+</div>
 
 @endsection
-
-@push('scripts_page')
-<script>
-
-</script>
-@endpush
